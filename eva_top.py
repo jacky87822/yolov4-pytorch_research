5e0c909de9f08af3384e086e12760a40535c8b89
jacky87822@gmail.com <jacky87822@gmail.com>
2022-09-04 09:35:17 +0800

    modified:   .gitignore 	modified:   eva_top.py

--- eva_top.py
+++ eva_top.py
@@ -23,12 +23,12 @@ for i in blind_list:
 os.system("python evaluate_on_coco.py -w ../model/yolov4-tiny.pt")
 '''
 
-sa_list=[0,1]
+sa_list=[1,0]
 for sa in sa_list:
     blind_list=[5,10,16,17,18,19,20,21,22,23]
     name = 'yolov4'
     for i in blind_list:
-        FILE_half="../model/full_"+i+"_b"+str(sa)+"_"+str(name)+".pt"
+        FILE_half="../model/full_"+str(name)+"_b"+str(sa)+"_"+str(i)+".pt"
         os.system("python evaluate_on_coco.py -w "+FILE_half)
 os.system("python evaluate_on_coco.py -w ../model/yolov4.pt")
 
